22edd5383036d4651ac87d2f57c08f9b6086f8d7
Julian Duque <julianduquej@gmail.com>
2020-08-06 12:27:04 -0400

    Update events API

--- src/server/events/index.js
+++ src/server/events/index.js
@@ -2,27 +2,27 @@ const { Router } = require('express');
 
 const router = new Router();
 
-router.get('/events', async (req, res) => {
+router.get('/', async (req, res) => {
     res.send('events');
 });
 
-router.post('/event', (req, res) => {
+router.post('/', (req, res) => {
     res.send('new event');
 });
 
-router.delete('/event/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     res.send('delete event');
 });
 
-router.put('/event/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     res.send('update event');
 });
 
-router.get('/event/:id/rsvp', (req, res) => {
+router.get('/:id/rsvps', (req, res) => {
     res.send('rsvp list for event');
 });
 
-router.post('/event/:id/rsvp/:memberId', (req, res) => {
+router.post('/:id/rsvps/:memberId', (req, res) => {
     res.send('rsvp for member');
 });
 
